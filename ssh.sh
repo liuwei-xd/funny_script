aba5d57b15b3587a34ff0f01ea10e62f30cac3a8
heavyhuang <heavyhuang@gmail.com>
2015-05-26 11:26:50 +0800

    fix some bug

--- ssh.sh
+++ ssh.sh
@@ -3,13 +3,13 @@ echo "hostname:"
 read NAME
 if [ "$NAME" == "lw" ]
    then
-#ssh-copy-id lw@219.245.80.21 
+#ssh-copy-id lw@192.168.1.1 
 set timeout 300
-ssh lw@219.245.80.21
+ssh lw@192.168.1.1
 
 elif [ "$NAME" == "sc" ]
    then
-#ssh-copy-id sc@219.245.80.29
+#ssh-copy-id sc@192.168.1.1
 set timeout 300
-ssh sc@219.245.80.29
+ssh sc@192.168.1.1
 fi
